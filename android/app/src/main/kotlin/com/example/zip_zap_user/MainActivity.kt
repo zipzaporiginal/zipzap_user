96bce9df8fa1437008a8173d834d2706355735a9
swabee <116912125+swabee@users.noreply.github.com>
2024-02-13 14:30:25 +0530

    first

---
+++ android/app/src/main/kotlin/com/example/zip_zap_user/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zip_zap_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Features/Base/base_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatefulWidget {
+  const BaseScreen({super.key});
+
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home'),
+    );
+  }
+}
